198f6e84be1dae7b980318a29b9591c9ea992141
Narenthar <narenthar31@gmail.com>
2022-07-26 22:24:29 +0530

    router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
         <LandingPage />
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
