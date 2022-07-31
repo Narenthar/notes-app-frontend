4d08a775a5e606846c0f5369fda90f2c66a64506
Narenthar <narenthar31@gmail.com>
2022-08-01 02:04:41 +0530

    logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,9 @@ import {
   FormControl,
   Container,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
@@ -34,7 +35,14 @@ const Header = () => {
               <NavDropdown.Item href="#action/3.1">Narenthar</NavDropdown.Item>
 
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  localStorage.removeItem("userInfo");
+                  navigate("/");
+                }}
+              >
+                Logout
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
